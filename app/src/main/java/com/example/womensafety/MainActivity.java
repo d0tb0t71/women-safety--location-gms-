0c4adaf83f242ac2121c100b7f3d4f253b4859ee
d0tb0t71 <sdipon.sutradhar@gmail.com>
2022-02-15 17:37:59 +0600

    C2

--- app/src/main/java/com/example/womensafety/MainActivity.java
+++ app/src/main/java/com/example/womensafety/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,12 +19,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Button get_help, show_location;
     TextView print_Location;
 
     LocationManager locationManager;
+    String lon = "";
+    String lat = "";
+    List<Address> addresses ;
+    String address;
 
 
     @Override
@@ -56,27 +66,36 @@ public class MainActivity extends AppCompatActivity {
                 print_Location.setText(String.valueOf(location.getLongitude()) + "\n" + String.valueOf(location.getLatitude()));
 
 
-                String lon = String.valueOf(location.getLongitude());
-                String lat = String.valueOf(location.getLatitude());
+                lon = String.valueOf(location.getLongitude());
+                lat = String.valueOf(location.getLatitude());
                 show_location.setEnabled(true);
 
-                show_location.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+                Geocoder geocoder;
+                geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                try {
+                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    address = addresses.get(0).getAddressLine(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-
-                        //Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
-                        Uri gmmIntentUri = Uri.parse("geo:" +lon+","+lat);
-                                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                        mapIntent.setPackage("com.google.android.apps.maps");
-                        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-                            startActivity(mapIntent);
-                        }
-                    }
-                });
 
             }
 
+        });
+
+        show_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Uri gmmIntentUri = Uri.parse("geo:" +lat+","+lon+"?q="+Uri.parse(lat+","+lon+"("+address+")"));
+                // Creates an Intent that will load a map of San Francisco
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
         });
 
 
